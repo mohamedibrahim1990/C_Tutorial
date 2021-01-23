009f62dbf291708b59afbfc7e490fb61d2e7ece4
mohamedibrahim51990 <mohamedibrahim51990@gmail.com>
2021-01-23 12:07:45 +0200

    inital commit

---
+++ 1-Overview of the language/1- C tutorial/file.c
@@ -0,0 +1,13 @@
+/* Author: www.w3schools.in
+Date: 2018-04-28
+Description:
+Writes the words "Hello World" on the screen */
+
+#include<stdio.h>
+
+int main() 
+{
+    printf("Hello world\n");
+    getch(); //Use to get one character input from user, and it will not be printed on screen
+    return 0;
+}
